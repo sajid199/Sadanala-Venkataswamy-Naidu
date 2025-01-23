13862738d34e43720e587b4de12a32549dec2166
sajid199 <sajideee2015@gmail.com>
2025-01-23 07:48:35 +0530

    loader flag chaned

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,10 +66,8 @@
     window.addEventListener('load', () => {
       setTimeout(() => {
         preloader.classList.add('loaded');
-      }, 1000);
-      setTimeout(() => {
         preloader.remove();
-      }, 2000);
+      }, 500);
     });
   }
 
